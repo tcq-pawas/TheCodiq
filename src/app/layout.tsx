a575c480ec030c0da62c56b4d824d13254a3958c
tcq-pawas <pawas.singh@thecodiq.com>
2026-07-15 17:47:13 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <script
           type="application/ld+json"
